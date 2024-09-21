e147fbd37774a14bc745b5d771bcd73092fdf559
Naviikr <naviikr4@gmail.com>
2024-09-21 23:57:14 +0530

    intial commit

---
+++ src/app/ui/pages/documents/taru/taru.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { PrivacyComponent } from './privacy/privacy.component';
+// import { DefaultComponent } from './default/default.component';
+import { RouterModule } from '@angular/router';
+// import { DeletionComponent } from './deletion/deletion.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { TaruPrivacyComponent } from './taru-privacy/taru-privacy.component';
+
+const routes =RouterModule.forChild( [
+
+  {
+    path: "privacy",
+    component: TaruPrivacyComponent
+  },
+]);
+
+@NgModule({
+  declarations: [
+  
+    TaruPrivacyComponent
+  ],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    routes
+  ]
+})
+export class TaruModule { }
